e78500ca8492367f48c390ea8ea81741972d7335
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2023-06-01 15:39:41 +0900

    min-rt が動いた！！

--- AArch64/libmincaml.c
+++ AArch64/libmincaml.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 // NOTE: コンパイル後のシンボルのプリフィックスに _ がつくのを避ける
 void min_caml_print_int(long n) asm("min_caml_print_int");
@@ -6,6 +7,18 @@ void min_caml_print_newline() asm("min_caml_print_newline");
 long* min_caml_create_array(long number_of_element, long init_value) asm("min_caml_create_array");
 double* min_caml_create_float_array(long number_of_element, double float_value) asm("min_caml_create_float_array");
 long min_caml_truncate() asm("min_caml_truncate");
+void min_caml_print_float(double d) asm("min_caml_print_float");
+void min_caml_print_byte(long n) asm("min_caml_print_byte");
+long min_caml_read_int() asm("min_caml_read_int");
+double min_caml_read_float() asm("min_caml_read_float");
+double min_caml_atan(double x) asm("min_caml_atan");
+double min_caml_cos(double x) asm("min_caml_cos");
+double min_caml_floor(double x) asm("min_caml_floor");
+double min_caml_sin(double x) asm("min_caml_sin");
+double min_caml_abs_float(double x) asm("min_caml_abs_float");
+double min_caml_float_of_int(long n) asm("min_caml_float_of_int");
+long min_caml_int_of_float(double d) asm("min_caml_int_of_float");
+double min_caml_sqrt(double d) asm("min_caml_sqrt");
 
 void min_caml_print_int(long n) {
   printf("%ld", n);
@@ -43,7 +56,7 @@ double* min_caml_create_float_array(long number_of_element, double float_value) 
   asm volatile ("mov %0, x27" : "=r"(heap_ptr));
 
   // Array の先頭アドレスを取得
-  long *array_ptr = heap_ptr;
+  double *array_ptr = heap_ptr;
 
   for (long i = 0l; i < number_of_element; i++) {
     // Array へ書き込んだ後、ヒープの先頭アドレスを8バイト進める
@@ -60,4 +73,66 @@ double* min_caml_create_float_array(long number_of_element, double float_value) 
 // truncate
 long min_caml_truncate(double d) {
   return (long)d;
+}
+
+void min_caml_print_float(double d) {
+  printf("%lf", d);
+}
+
+void min_caml_print_byte(long n) {
+  putchar(n);
+}
+
+long min_caml_read_int() {
+  long l;
+  // fscanf(fp, "%ld", &l);
+  scanf("%ld", &l);
+  return l;
+}
+
+double min_caml_read_float() {
+    double d;
+    // fscanf(fp, "%lf", &d);
+    scanf("%lf", &d);
+    return d;
+}
+
+// atan
+double min_caml_atan(double x) {
+  return atan(x);
+}
+
+// cos
+double min_caml_cos(double x) {
+  return cos(x);
+}
+
+// floor
+double min_caml_floor(double x) {
+  return floor(x);
+}
+
+// sin
+double min_caml_sin(double x) {
+  return sin(x);
+}
+
+// abs_float
+double min_caml_abs_float(double x) {
+  return fabs(x);
+}
+
+// float_of_int
+double min_caml_float_of_int(long n) {
+  return (double)n;
+}
+
+// int_of_float
+long min_caml_int_of_float(double d) {
+  return (long)d;
+}
+
+// sqrt
+double min_caml_sqrt(double d) {
+  return sqrt(d);
 }
